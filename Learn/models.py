6ac3f42ba2e4600da3c22e4990a05e47688dc35f
Benedict <newlyf38@gmail.com>
2023-02-06 11:26:15 +0000

    created the User model for abstractUser class

--- Learn/models.py
+++ Learn/models.py
@@ -5,7 +5,10 @@ from django.contrib.auth import get_user_model
 from embed_video.fields import EmbedVideoField
 
 
- 
+class User(AbstractUser):
+    is_learner = models.BooleanField(default=False)
+    is_instructor = models.BooleanField(default=False)
+    is_admin = models.BooleanField(default=False)
 
 
 # Create your models here.
