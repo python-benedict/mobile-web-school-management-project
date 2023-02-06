eecb427ba7fad5c83f9b2ad2ab4f41c904b4148f
Benedict <newlyf38@gmail.com>
2023-02-06 11:46:12 +0000

    added the anouncement models to the project

--- Learn/models.py
+++ Learn/models.py
@@ -31,3 +31,12 @@ class Profile(models.Model):
     height = models.CharField(max_length=50)
     level =models.CharField(max_length=50)
     profession = models.CharField(max_length=50, default='Student')
+
+class Announcement(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    posted_at = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.content
+    
