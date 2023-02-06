a99f9a0c34825fee1ba732591432f9592d65b01d
Benedict <newlyf38@gmail.com>
2023-02-06 15:27:59 +0000

    added a learner class model to the project

--- Learn/models.py
+++ Learn/models.py
@@ -113,6 +113,19 @@ class Answer(models.Model):
         return self.text
     
 
+class Learner(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    quizzes = models.ManyToManyField(Quiz, through='TakenQuiz')
+    interests = models.ManyToManyField(Course, related_name='interested_learners')
 
+    def get_unanswered_questions(self, quiz):
+        answered_questions = self.quiz_answers \
+            .filter(answer__question__quiz=quiz) \
+            .values_list('answer__question__pk', flat=True)
+        questions = quiz.questions.exclude(pk__in=answered_questions).order_by('text')
+        return questions
+
+    def __str__(self):
+        return self.user.username
     
     
