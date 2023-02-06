d50664c8479adc7484b6c2a81d15a9fe9fcac10d
Benedict <newlyf38@gmail.com>
2023-02-06 11:14:05 +0000

    added another configurations to the settings file

--- SchoolManagementSystem/settings.py
+++ SchoolManagementSystem/settings.py
@@ -134,3 +134,13 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
+
+LOGIN_URL = 'login_form'
